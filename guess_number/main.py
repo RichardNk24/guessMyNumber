fe48bcf2d8afcfc3b2311136c9a2c7c1e0ac6eb8
Richard Nkulu <64165035+RichardNk24@users.noreply.github.com>
2022-05-05 12:44:06 +0200

    Find the largest number

--- guess_number/main.py
+++ guess_number/main.py
@@ -27,3 +27,22 @@ def computer_guess(x):
                 low = guess + 1
         print (f"Yaay! The computer guessed your number, {guess}, correctly!")
 computer_guess(500)
+
+
+# Find the largest number among the three input numbers
+num1 = 10
+num2 = 14
+num3 = 12
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+if (num1 >= num2) and (num1 >= num3):
+   largest = num1
+elif (num2 >= num1) and (num2 >= num3):
+   largest = num2
+else:
+   largest = num3
+
+print("The largest number is", largest)
