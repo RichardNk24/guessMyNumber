badab08fdb72889e4e05b4b7ca3bd3016c95d043
Richard Nkulu <64165035+RichardNk24@users.noreply.github.com>
2022-05-05 12:45:18 +0200

    Check if the input number is odd or even

--- guess_number/main.py
+++ guess_number/main.py
@@ -46,3 +46,12 @@ else:
    largest = num3
 
 print("The largest number is", largest)
+
+
+# Check if the input number is odd or even.
+
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
